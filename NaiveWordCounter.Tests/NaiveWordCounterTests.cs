08a9563d652f7d609af4d83dfc8e65a8f4397131
TomDane <tjdane@gmail.com>
2016-10-14 17:30:26 +0100

    add nunit, verify it is working

---
+++ NaiveWordCounter.Tests/NaiveWordCounterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NaiveWordCounter.Tests
+{
+	[TestFixture]
+    public class NaiveWordCounterTests
+    {
+		[Test]
+		public void HelloNunit()
+		{
+			Assert.Pass();
+		}
+    }
+}
