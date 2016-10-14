8a5dafe436ebfadd11bc4df061feb59c03681d99
TomDane <tjdane@gmail.com>
2016-10-14 17:41:18 +0100

    add first test. it is failing as no method exists

--- NaiveWordCounter.Tests/NaiveWordCounterTests.cs
+++ NaiveWordCounter.Tests/NaiveWordCounterTests.cs
@@ -15,5 +15,20 @@ namespace NaiveWordCounter.Tests
 		{
 			Assert.Pass();
 		}
+
+		//I am going to be committing more than usual here. I want to show thought process. In production or at work,
+		//I would prefer to rebease all of this into a single clean commit before merging to master. 
+
+		[Test]
+		public void ReadTextFile_ShouldReturnText_WhenProvidedTxtFile()
+		{
+			//Arrange
+			var text = "the quick brown fox";
+
+			//Act
+			var result = ReadTextFile(pathToFile); //I may have to address porblems loading extremely long books into memory like this. 
+
+			Assert.AreEqual(text, result);
+		}
     }
 }
---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Notes
+
+Optimizations and scaling
+
+1. What if we wanted to process multiple books at once? Could use async or multithreading. 
+2. Do we need to load entire book into memory? Could we parse it bit by bit? 
