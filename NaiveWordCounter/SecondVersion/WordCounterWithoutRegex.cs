bb382f51d9160d98d3fc36296c8b629f884330c3
TomDane <tjdane@gmail.com>
2016-10-17 18:58:07 +0100

    test breaking, issue with word count

---
+++ NaiveWordCounter/SecondVersion/WordCounterWithoutRegex.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Text;
+using NaiveWordCounter.Interfaces;
+
+namespace NaiveWordCounter
+{
+	public class WordCounterWithoutRegex : IWordCounter
+	{
+		public IDictionary<string, int> Count(string[] linesOfText)
+		{
+			var result = new ConcurrentDictionary<string, int>();
+			//var regex = new Regex("[^\\p{L}]"); //matches all unicode letters (latin and non-latin)
+
+			Parallel.For(0, linesOfText.Length, x =>
+			{
+				var line = linesOfText[x];
+				var lineChunks = line.Split(' ');
+
+				foreach (var chunk in lineChunks)
+				{
+					var stringBuilder = new StringBuilder();
+
+					foreach (var item in chunk)
+					{
+						if (char.IsLetter(item))
+						{
+							stringBuilder.Append(item);
+						}
+					}
+
+					var word = stringBuilder.ToString().ToLower();
+
+					if (!string.IsNullOrEmpty(word.ToString()))
+					{
+						result.AddOrUpdate(word, 1, (key, value) => value + 1);
+						//AddOrUpdate: 
+						//Add 'word' (first parameter) with a value of 1 (second parameter) if it does NOT exist
+						//Otherwise, add it as a new key, and add 1 to its value (third param)
+						//Explanation here https://www.dotnetperls.com/concurrentdictionary
+					}
+				}
+			});
+
+			var orderedResult = result.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(i => i.Key, i => i.Value);
+			return orderedResult;
+		}
+	}
+}
